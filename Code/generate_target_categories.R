efc15d7fdf67d0722f6ea5a65378a2e0865bd8ff
smathis <66260252+smathis14@users.noreply.github.com>
2024-05-23 10:22:32 -0400

    reconfigure how to read in data

--- Code/generate_target_categories.R
+++ Code/generate_target_categories.R
@@ -2,76 +2,64 @@
 
 
 library(tidyverse)
-library(zoo)
-library(RSocrata)
+
 #pull target data 
 
-#this function is the fetch flu function from get_target_data that is used for FluSight GitHub
-#this pulls the final NHSN update from "https://healthdata.gov/Hospital/COVID-19-Reported-Patient-Impact-and-Hospital-Capa/g62h-syeh"
-## including this archive in case the above link stops functioning in the future 
-# "https://healthdata.gov/dataset/COVID-19-Reported-Patient-Impact-and-Hospital-Capa/qqte-vkut/about_data"
+## 2022-2023
+target_23 <- read.csv("https://raw.githubusercontent.com/cdcepi/Flusight-forecast-data/master/data-truth/truth-Incident%20Hospitalizations.csv")
 
-fetch_flu <- function(temporal_resolution = "weekly", na.rm = TRUE){
-  require(dplyr)
-  require(lubridate)
-  require(RSocrata)
-  
-  #read data from healthdata.gov, filtering for when flu reporting became mandatory
-  health_data = RSocrata::read.socrata(url = "https://healthdata.gov/resource/g62h-syeh.json") %>% 
-    dplyr::filter(date >= as.Date("2022-02-02"))
-  
-  #remove  VI and AS as they are not included for FluSight, keep only necessary vars and add epiweek and epiyear 
-  recent_data = health_data %>% 
-    dplyr::filter(!state %in% c("VI", "AS")) %>% 
-    dplyr::select(state, date, previous_day_admission_influenza_confirmed) %>% 
-    dplyr::rename("value" = "previous_day_admission_influenza_confirmed") %>% 
-    dplyr::mutate(date = as.Date(date), 
-                  value = as.numeric(value),
-                  epiweek = lubridate::epiweek(date), 
-                  epiyear = lubridate::epiyear(date))
-  
-  #summarize US Flu 
-  us_data = recent_data %>% dplyr::group_by(date, epiweek, epiyear) %>% 
-    dplyr::summarise(value = sum(value, na.rm = na.rm)) %>% 
-    dplyr::mutate(state = "US") %>% 
-    dplyr::ungroup()
-  
-  #bind state and US data
-  full_data = rbind(recent_data, us_data) %>% 
-    dplyr::left_join(., locations, by = join_by("state" == "abbreviation"))
-  
-  #convert counts to weekly and calculates weekly rate 
-  weeklydat = full_data %>% 
-    dplyr::group_by(state,epiweek,epiyear, location, location_name, population) %>% 
-    dplyr::summarise(value = sum(value, na.rm = na.rm), date = max(date), num_days = n()) %>% 
-    dplyr::ungroup() %>% 
-    dplyr::filter(num_days == 7L) %>% 
-    dplyr::select(-num_days, -epiweek, -epiyear) %>% 
-    dplyr::mutate(weekly_rate = (value*100000)/population )
-  
-  #if daily data is ever wanted, this returns correct final data
-  if(temporal_resolution == "weekly"){
-    final_dat = weeklydat %>% 
-      dplyr::select(date, location, location_name, value, weekly_rate) %>% 
-      dplyr::arrange(desc(date))
-  } else{
-    final_dat = full_data 
-  }
-  return(final_dat)
-  
-}
 
-#location data is necessary to use fetch_flu function, must be named locations
-locations <- read.csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>% dplyr::select(1:4)
+#location data (2021-2022/2022-2023)
+locations_23 <-read.csv("https://raw.githubusercontent.com/cdcepi/Flusight-forecast-data/master/data-locations/locations.csv") %>%
+  mutate(count_rate1=NULL,count_rate2=NULL,location=NULL)
 
-target_data <- fetch_flu(temporal_resolution = "weekly")
+obs_23 <- target_23 %>%  
+  filter(date >= as.Date("2022-10-03"), date <= as.Date("2023-06-10")) %>% 
+  dplyr::inner_join(locations_23,
+                    by = c("location_name"))  %>% 
+  filter(date > as.Date("2022-02-20"), abbreviation != "VI") %>%
+  mutate(weekly_rate = value*100000/population) %>% 
+  
+  group_by(location_name) %>% 
+  arrange(date) %>% 
+  mutate(rate_diff = weekly_rate-lag(weekly_rate, 2),
+         count_change = value - lag(value, 2), 
+         count = value) %>% 
+  ungroup() %>% 
+  group_by(location_name,population) %>%
+  mutate(category = ifelse(abs(count_change) <20, "stable",NA)) %>%
+  mutate(category = ifelse(abs(rate_diff)<1, "stable",category)) %>%
+  mutate(category = ifelse((abs(rate_diff) >1 & abs(count_change) <20), "stable",category)) %>%
+  mutate(category = ifelse((rate_diff >1 & abs(count_change) >=20),"increase",category)) %>%
+  mutate(category = ifelse((rate_diff >2 & abs(count_change) >=40),"large_increase",category)) %>%
+  mutate(category = ifelse((rate_diff <(-1) & abs(count_change) >=20),"decrease",category)) %>%
+  mutate(category = ifelse((rate_diff <(-2) & abs(count_change) >=40),"large_decrease",category)) %>% 
+  mutate(horizon = 1, 
+         rate_diff = rate_diff, 
+         date = as.Date(date), 
+         season = 23) %>% 
+  ungroup()  %>% 
+  mutate(numeric_category := factor(category, 
+                                    levels= c("large_decrease", "decrease", "stable",
+                                              "increase", "large_increase"),
+                                    labels = seq(-2,2)) ) %>% 
+  select(date, location_name, location, horizon, rate_diff, category, season, weekly_rate, count, numeric_category)
+
+
+## 2023-2024
+target_24 <-  readr::read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/target-data/target-hospital-admissions.csv") %>% select(-`...1`)
+
+# load location data 
+locations_24 <- readr::read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>%
+  dplyr::select(location,location_name, count_rate1, count_rate2, count_rate2p5, count_rate3, count_rate4, count_rate5)
+
 
 #filter for beginning of FluSight Season
 #join with locations, get rates diffs, set categories
-obs_24 <- target_data %>% 
+obs_24 <- target_24 %>% 
   filter(date >= as.Date("2023-10-01")) %>%
   select(-c(location))%>%
-  dplyr::inner_join(locations,
+  dplyr::inner_join(locations_24,
                     by = c("location_name"))  %>% 
   group_by(location_name) %>% 
   arrange(date) %>% 
@@ -107,11 +95,18 @@ obs_24 <- target_data %>%
                               horizon == 3 & rate_diff <= -2.5 ~ "decrease"), 
          season = 24) %>% 
   rename(count = value) %>% 
-  select(date, location_name, location, horizon, rate_diff, category, season, weekly_rate, count)
-
-obs_data_with_numcat <- obs_24 %>% 
+  select(date, location_name, location, horizon, rate_diff, category, season, weekly_rate, count) %>% 
   mutate(numeric_category := factor(category, 
                                     levels= c("large_decrease", "decrease", "stable",
                                               "increase", "large_increase"),
                                     labels = seq(-2,2)) )
-write.csv(obs_data_with_numcat,  paste0("C:/Users/", Sys.info()["user"], "/Desktop/GitHub/FluSight-categorical/Target_data/obs_24_with_numcat.csv"), row.names = FALSE)
+
+
+
+
+obs_data_with_numcat <- bind_rows(obs_23, obs_24)
+
+# write.csv(obs_data_with_numcat,  paste0("C:/Users/", Sys.info()["user"], "/Desktop/GitHub/FluSight-categorical/Target_data/obs_data_with_numcat.csv"), row.names = FALSE)
+
+
+
